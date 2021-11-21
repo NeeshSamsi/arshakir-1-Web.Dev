510fbeffa94bbe5332481ffe6139d5984f501ce6
Neesh <avaneeshsamsi@gmail.com>
2021-11-21 13:12:41 +0530

    feat: added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   purge: ["./index.html"],
@@ -7,7 +9,22 @@ module.exports = {
       inter: ["Inter", "sans-serif"],
       gilroy: ["Gilroy", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          primary: "#313131",
+          secondary: "#514040",
+          "testimonial-primary": "#32435C",
+          "testimonial-secondary": "#6A635B",
+        },
+        light: "#F2F2F2",
+        accent: "#49AD09",
+        card: {
+          primary: colors.white,
+          secondary: "#EDEDED",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
